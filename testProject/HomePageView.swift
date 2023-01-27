792373523d373785f9a9e72e497ae6f43f258501
isler <chrislermedua@gmail.com>
2023-01-27 16:16:01 +1100

    update refactor

--- testProject/HomePageView.swift
+++ testProject/HomePageView.swift
@@ -17,43 +17,56 @@ struct Hero: Codable {
     let localized_name: String
     let primary_attr: String
     let icon: String
+    let img: String
+    let attack_type: String
 }
 
 
 struct HomePageView: View {
-    let backgroundGradient = LinearGradient(
-        gradient: Gradient(colors: [Color.pink, Color.yellow]),
-        startPoint: .top, endPoint: .bottom)
+    @State private var heroes: [Hero] = []
     
     var body: some View {
-        
         NavigationView {
             
-            if #available(iOS 16.0, *) {
-                ZStack {
-                    backgroundGradient.ignoresSafeArea()
-                    //Color.red.edgesIgnoringSafeArea(.all)
-                    heroList()
-                }
-                .navigationTitle("DOTA HERO")
-                .toolbarBackground( Color.pink, for: .navigationBar)
-            } else {
+            ZStack{
+                LinearGradient(
+                    gradient: Gradient(colors: [.white, Color("lightGray"), .gray]),
+                    startPoint: .top, endPoint: .bottom)
+                .edgesIgnoringSafeArea(.all)
                 
+                heroList(heroes: $heroes)
+            }
+            .navigationTitle("DOTA HERO")
+        }
+        .onAppear {
+            
+            AF.request(DemoURLs.baseURL + "/api/heroStats").response { response in
+                switch response.result {
+                case .success(let data):
+                    do {
+                        let jsonData = try JSONDecoder().decode([Hero].self, from: data!)
+                        print(jsonData)
+                        self.heroes = jsonData
+                    }catch {
+                        print(error.localizedDescription)
+                    }
+                case .failure(let errors):
+                    print(errors.localizedDescription)
+                }
             }
         }
     }
-        
-    }
+}
     
-    struct HomePageView_Previews: PreviewProvider {
-        static var previews: some View {
-            HomePageView()
-        }
+struct HomePageView_Previews: PreviewProvider {
+    static var previews: some View {
+        HomePageView()
     }
+}
 
 
 struct heroList: View {
-    @State var heroes: [Hero] = []
+    @Binding var heroes: [Hero]
     var body: some View {
         ScrollView(.vertical, showsIndicators: false) {
             VStack (alignment: .leading){
@@ -87,23 +100,6 @@ struct heroList: View {
                             }
                         }
                     }
-                }
-            }
-        }
-        .onAppear {
-            
-            AF.request(DemoURLs.baseURL + "/api/heroStats").response { response in
-                switch response.result {
-                case .success(let data):
-                    do {
-                        let jsonData = try JSONDecoder().decode([Hero].self, from: data!)
-                        print(jsonData)
-                        self.heroes = jsonData
-                    }catch {
-                        print(error.localizedDescription)
-                    }
-                case .failure(let errors):
-                    print(errors.localizedDescription)
                 }
             }
         }
